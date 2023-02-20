bdf0fbef29c1a048b7dbe34526fc800b7df1640c
akameshi <bogdan.kuzmin.08@gmail.com>
2023-02-20 22:25:17 +0600

    Create homework-3.py

---
+++ homework-3.py
@@ -0,0 +1,21 @@
+print("ДЗ-1 -----------------FOR---------------- ДЗ-1")
+count=2
+num=0
+for i in range(2,6):
+    print(i)
+i=0
+print("ДЗ-1 -----------------WHILE---------------- ДЗ-1")
+while count < 6:
+    print(count)
+    count+=1
+count=0
+print("ДЗ-2 -----------------FOR---------------- ДЗ-2")
+for i in range(6):
+    print(num)
+    num+=5
+num=0
+print("ДЗ-2 -----------------WHILE---------------- ДЗ-2")
+while count < 6:
+    print(num)
+    num+=5
+    count+=1
